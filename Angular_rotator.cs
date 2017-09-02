6c1dc697a29df4fd4124cd817e80d4eff6223660
girishreddy929 <31560411+girishreddy929@users.noreply.github.com>
2017-09-02 12:26:33 +0530

    Add files via upload

---
+++ Angular_rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angular_rotator : MonoBehaviour {
+    public Rigidbody rb;
+    public float tumble;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
---
+++ Destroy_By_Boundary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_By_Boundary : MonoBehaviour {
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
